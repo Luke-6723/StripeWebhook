897c6705fdb9842824d1da04790ed8d3d14e9808
Luke Stoodley <luke@ichigo.uk>
2025-04-29 22:14:47 +0200

    checkout tests

---
+++ src/test/checkout.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from "vitest";
+import request from "supertest";
+import { app } from "../server.js";
+
+describe("POST /api/checkout", () => {
+  it("A valid checkout post should return a checkout URL", async () => {
+    const response = await request(app)
+      .post("/api/checkout")
+      .send({
+        "email": "luke@ichigo.uk",
+        "userId": "this_is_a_test",
+        "lineItems": [
+          {
+            "id": "prod_SDkfwwn0CqiYj9"
+          }
+        ]
+      })
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("url");
+  });
+});
