a7bb73e0070c8d61f3ecf7617bf68b7b6977ccc6
Luke Stoodley <luke@ichigo.uk>
2025-07-29 18:49:05 +0100

    Add test case for checkout post with parameters including allowCoupon and consentCollection

--- src/test/checkout.test.ts
+++ src/test/checkout.test.ts
@@ -23,4 +23,34 @@ describe("POST /api/checkout", () => {
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("url");
   });
+
+  it("A valid checkout post should return a checkout URL with params set", async () => {
+    const response = await request(app)
+      .post("/api/checkout")
+      .send({
+        "successRedirect": "http://localhost:3000/success",
+        "cancelRedirect": "http://localhost:3000/cancel",
+        "email": "automated_testing@ichigo.uk",
+        "userId": "automated_testing",
+        "allowCoupon": true,
+        "consentCollection": {
+          "payment_method_reuse_agreement": { "position": "hidden" },
+          "terms_of_service": "required"
+        },
+        "customText": {
+          "after_submit": { "message": "Thank you for your purchase!" },
+          "terms_of_service_acceptance": { "message": "I agree to the terms of service." }
+        },
+        "lineItems": [
+          {
+            "id": "prod_SDkfwwn0CqiYj9"
+          }
+        ]
+      })
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("url");
+  });
 });
